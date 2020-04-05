3fd59b6c12ff020e548d9be647e5175e5026f69f
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2020-04-05 20:42:06 -0300

    adicao do item 27 e inicio do 28

---
+++ Aula 6/27 numero harmonico.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+har = 0
+for c in range(1, num+1):
+    har = har + 1 / c
+print(har)
---
+++ Aula 6/28 ler valor e calcular.py
@@ -0,0 +1,7 @@
+valor = int(input('Digite um valor: '))
+#e = 1 + 1 / N
+for c in range(1, valor+1):
+    for item in c:
+        print(item)
+    #e = 1 + 1 / c
+    #print(c)
