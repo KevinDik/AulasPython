8e14421079915797bd6f82e06bd4e462155b8df2
Kevin Diego <kevindiegodasilvasousa@gmail.com>
2020-04-19 15:17:08 -0300

    finalização do excercicio 24
    mudança da escrita do codigo para funcionamento mais liso

--- Aula 7/24.py
+++ Aula 7/24.py
@@ -1,17 +1,7 @@
 from random import triangular
 alunos = []
-cadastro = {}
 for pessoa in range(1, 11):
-    cadastro['Nº'] = pessoa
-    cadastro['Altura'] = triangular(1.5, 2.0)
-    alunos.append(cadastro.copy())
-    cadastro.clear()
-controle = 0
+    alunos.append(triangular(1.5, 2.0))
 print(alunos)
-for pessoa in alunos:
-    if max(alunos[controle]["Altura"]):
-        print(f'O maior aluno corresponde ao aluno nº {0} com a altura {alunos[controle]["Altura"]}')
-    if min(alunos[controle]["Altura"]):
-        print(f'O menor aluno corresponde ao aluno nº {0} com a altura {alunos[controle]["Altura"]}')
-    controle += 1
-
+print(f'Maior aluno: {alunos.index(max(alunos))}, com o tamanho de {max(alunos):.2f}')
+print(f'Menor aluno: {alunos.index(min(alunos))}, com o tamanho de {min(alunos):.2f}')
